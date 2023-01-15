8b159db3ff698c798aafda4cb060bc87faae07ce
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-01-15 21:40:16 +0000

    working on bulk create function

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const User = require('./User');
 const Comments = require('./Comments');
 const Posts = require('./Post');
-const Upvotes = require('./Upvotes');
+//const Upvotes = require('./Upvotes');
 
 
 User.hasMany(Posts, {
@@ -14,10 +14,10 @@ Posts.belongsTo(User, {
 
 });
 
-Posts.hasMany(Upvotes, {
+Posts.hasMany(Comments, {
     //not sure about key
     foreignKey: 'user_id'
-
+    
 })
 
-module.exports = { User, Posts, Upvotes, };
+module.exports = { User, Posts, Comments };
