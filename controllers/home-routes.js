d67776dc7af3e677c223bccd9ea1d819e65e614f
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2023-01-17 19:28:55 +0000

    changed home routes location

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,2 +1,27 @@
 const router = require('express').Router();
-const { User, Upvotes, Posts, Comments } = require('../models');
+const { Posts } = require('../models');
+
+router.get('/', async (req, res) => {
+    try {
+      const postData = await Posts.findAll({
+        //include: [
+          //{
+            //model: User,
+            //attributes: ['name'],
+         // },
+        //],
+      });
+  
+      // Serialize data so the template can read it
+      const posts = postData.map((project) => project.get({ plain: true }));
+  
+      res.render('homepage', { 
+        posts, 
+        //logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+module.exports = router;
