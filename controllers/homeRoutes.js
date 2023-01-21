f7151421d80a81f450bc221acd3096960640b661
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2023-01-21 16:50:28 +0000

    began to add comment functions

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -48,8 +48,9 @@ router.get("/post/:id", async (req, res) => {
     });
 
     const post = postData.get({ plain: true });
+    console.log(post)
 
-    res.render("post", {
+    res.render("singlepost", {
       post,
       logged_in: req.session.logged_in
     });
