3de5903b252d7b51500087dd3df589ac6b23a4b0
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-01-21 19:37:56 +0000

    working on hiding and showing comments for a post

--- Public/js/index.js
+++ Public/js/index.js
@@ -1,12 +1,25 @@
 console.log('hello')
 
- const commentSection = document.getElementById("comment-section");
-const viewCommentBtn = document.getElementById("");
+const commentSection = document.getElementById("comment-section");
+const viewCommentBtn = document.getElementById("view-comment-btn");
 
- commentSection.style.display = "none";
+commentSection.style.visibility = "hidden";
+
+function showComments() {
+    commentSection.style.visibility = "visible";
+    viewCommentBtn.textContent = "Hide Comments";
+}
+
+function hideComments() {
+    if (viewCommentBtn.textContent ===  "Hide Comments")
+    {
+        commentSection.style.visibility = "hidden";
+
+    }
+}
 
 
-
+viewCommentBtn.addEventListener('click', showComments)
 
 
 
