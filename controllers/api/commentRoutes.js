4d852255354591b4f0adde67a3c335fa75935cc0
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2023-01-21 16:50:28 +0000

    began to add comment functions

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { Comments } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+      const newComment = await Comments.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newComment);
+      // console.info(newPost)
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+
+module.exports = router;
